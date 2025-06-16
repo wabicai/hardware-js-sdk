bd71f10c9265e4793cf6d77a543caabc7862c9be
wabicai <wabicai@users.noreply.github.com>
2025-06-16 02:55:28 +0000

    deploy: 1d29f2e1ab31153b9045b34a0bf4748de315b77d

---
+++ assets/lightning.ts-1d29f2e-C8y9mwQP.js
@@ -0,0 +1,2 @@
+const i={id:"lightning",name:"Lightning",description:"Bitcoin Lightning Network operations",icon:"",color:"#F7931A",category:"bitcoin"},t=[{method:"lnurlAuth",description:"LNURL Auth",presupposes:[{title:"LNURL Auth",value:{domain:"site.com",k1:"e2af6254a8df433264fa23f67eb8188635d15ce883e8fc020989d5f82ae6f11e"}}]}],e={...i,api:t};export{e as chainConfig,i as chainMeta,t as default};
+//# sourceMappingURL=lightning.ts-1d29f2e-C8y9mwQP.js.map
